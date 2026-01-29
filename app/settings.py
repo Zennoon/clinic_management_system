5d365225e86371ff19dd430d6a9a768bde38d69e
Zennoon <98653096+Zennoon@users.noreply.github.com>
2026-01-29 21:46:39 +0300

    Create a staff model to represent system users
    Implemented a new Staff model with a role field to represent staff
    members with different roles; namely: admins, doctors, nurses,
    receptionists, and laboratory technicians.

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'staff.apps.StaffConfig'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Custom settings
+AUTH_USER_MODEL = 'staff.Staff'
---
+++ staff/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django_enum import EnumField
+
+
+# Create your models here.
+class Staff(AbstractUser):
+    class RoleEnum(models.TextChoices):
+        ADMIN = 'ADMIN'
+        DOCTOR = 'DOCTOR'
+        NURSE = 'NURSE'
+        LABORATORY = 'LABORATORY'
+        RECEPTION = 'RECEPTION'
+        OTHER = 'OTHER'
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    role = EnumField(RoleEnum)
+
+    def __str__(self):
+        return f"{self.username}: {self.phone} <{self.role.name}>"
